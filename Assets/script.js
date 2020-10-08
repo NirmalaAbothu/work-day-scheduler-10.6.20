dd378b803bf07870a483fff59869dd5ad4efcf8a
Nirmala <nirmala_ira@hotmail.com>
2020-10-08 11:21:21 -0700

    added README file and did refactor the code

--- Assets/script.js
+++ Assets/script.js
@@ -1,91 +1,60 @@
+// DOM reference variables
 var rowTimeEL = $(".row");
 var scheduleEL = $(".schedule");
-// console.log(rowTimeEL);
 let currentHour = moment().format("H");
 console.log(currentHour);
 let currentDayEL = $("#currentDay");
-// currentDayEL.text(moment().format("LLLL"));
 currentDayEL.text(moment().format("ll"));
 var textAreaEL = $(".textarea");
-// $("#currentDay").text(moment().format('ddd, MMM Do')
-//each object has a hour property and textArea property
+
+// declare arraylist
 var toDoList = [];
-// $.each(rowTimeEL, function (i, rowtime) {
-//      let rowIdString = rowtime.id;
-//      console.log(rowIdString);
-//      if (rowIdString) var rowHour = parseInt(rowIdString);
-//      if (rowHour) {
-//           if (rowHour === currentHour) {
-//                //    console.log(currentHour);
-//                setColor(rowtime, "red");
-//                //   let rowHour = moment(rowId,"HH")
-//           } else if (currentHour < rowHour) {
-//                setColor(rowtime, "green");
-//           } else if (currentHour > rowHour) {
-//                setColor(rowtime, "lightgrey");
-//           }
-//      }
-// });
-function setColor(element, color) {
-     element.style.backgroundColor = color;
-}
-var startTime = "10 am";
-var endTime = "1 pm";
-var result = moment
-     .utc(moment(endTime, "HH:mm:ss").diff(moment(startTime, "HH:mm:ss")))
-     .format("HH");
-console.log(result);
-//set the colors depending on time
+
+//function to set the colors depending on time
 function setUpTimeBlockColors() {
      rowTimeEL.each(function () {
           var $thisBlock = $(this);
-          // let rowIdString = rowtime.id;
-          // console.log(rowIdString);
           var rowHour = parseInt($thisBlock.attr("data-hour"));
-          // let thisBlockHour = parseInt(rowIdString);
-          console.log(rowHour);
-          console.log(currentHour);
           if (rowHour == currentHour) {
                $thisBlock.addClass("present").removeClass("past future");
-               console.log("green");
           }
           if (rowHour < currentHour) {
                $thisBlock.addClass("past").removeClass("present future");
-               console.log("red");
           }
           if (rowHour > currentHour) {
                $thisBlock.addClass("future").removeClass("past present");
-               console.log("gray");
           }
      });
 }
-//initialize toDoList
+
+//initialize toDoList array
 function initializeToDoList() {
+     //iterating thru loop to retrieve time
      rowTimeEL.each(function () {
           var $thisBlock = $(this);
-          // let rowIdString = rowtime.id;
-          // console.log(rowIdString);
           var rowHour = parseInt($thisBlock.attr("data-hour"));
-          //set related hour as id(hour)
-          //set text to accept input string
+          // creating object
           var todoObj = {
                hour: rowHour,
                text: "",
           };
-          console.log("functioncalled");
-          //pust this object to toDoList array
+          //push this object to toDoList array
           toDoList.push(todoObj);
           console.log(toDoList);
      });
+     // storing the arraylist in localstorage
      localStorage.setItem("events", JSON.stringify(toDoList));
 }
-//function
+//function renderSchedule to display time and corresponding events
 function renderSchedule() {
+     // retrieveing the arraylist from localstorage
      toDoListItems = localStorage.getItem("events");
      console.log(toDoListItems);
+     // converting the arraylist from string to object
      var items = JSON.parse(toDoListItems);
      toDoList = items;
-     //loop thru
+     //loop thru to retrieve time and corresponding textarea which is stored in
+     // localstorage
      for (var i = 0; i < items.length; i++) {
           var itemHour = items[i].hour;
           console.log(itemHour);
@@ -102,20 +71,14 @@ function renderSchedule() {
                .children("textarea")
                .val(itemTextArea)
      );
-     // .val(itemTextArea);
-     // console.log(result);
      console.log($("[data-hour=" + itemHour + "]"));
      console.log(toDoList);
 }
-//saveItem
+
+//function saveItem will be called after user clicked save button
 function saveItem() {
      var $thisBlock = $(this);
-     console.log($thisBlock);
      var hourToUpdate = $(this).parent().parent().attr("data-hour");
-     // console.log($(this).parent());
-     // console.log($(this).parent().parent());
-     // console.log($(this).parent().parent().parent().children("textarea"));
-     console.log(hourToUpdate);
      var itemToSave = $(this)
           .parent()
           .parent()
@@ -123,20 +86,8 @@ function saveItem() {
           .children("textarea")
           .val();
      console.log("HERE", itemToSave);
-     // var hourToUpdate = $(this).parent().attr("data-hour");
-     // console.log(hourToUpdate);
-     // var itemToAdd = $(this).parent().children("textarea").val();
-     // console.log(itemToAdd);
-     // event.preventDefault();
-     // // var $thisBlock = $(this).parent();
-     // var hourToUpdate = $(this).parent().attr("data-hour");
-     // console.log(hourToUpdate);
-     // var itemToAdd = $(this).parent().children("textarea").val();
-     // console.log(itemToAdd);
-     // console.log(itemToSave);
-     console.log(this);
      console.log("NOW", toDoList);
-     //loop thru
+     //loop thru the arraylist to find the time block on which user saves event
      for (var i = 0; i < toDoList.length; i++) {
           //compare which item needs to be update based on hour clicked
           if (toDoList[i].hour == hourToUpdate) {
@@ -144,21 +95,24 @@ function saveItem() {
           }
      }
      console.log("HERE", toDoList, typeof toDoList);
+     //store the event in localstorage
      localStorage.setItem("events", JSON.stringify(toDoList));
      renderSchedule();
 }
 $(document).ready(function () {
      //calling setUPTimeBlockColors function
      setUpTimeBlockColors();
-     //if there isn no todoList in localstorage,then initialize list
-     // console.log(localStorage.getItem("todos"));
+     //if there is no todoList in localstorage,then initialize list
      if (!localStorage.getItem("events")) {
           initializeToDoList();
-     } else {
+     }
+     // if there is todoList in localstorage,then display the list
+     else {
           alert(localStorage.getItem("events"));
      }
      //render schedule from local storage
      renderSchedule();
-     //when item save button is clicked, save it
+     //when save button is clicked,calling saveItem function
+     // to saves the event in localstorage
      scheduleEL.on("click", "button", saveItem);
 });
--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # work-day-scheduler-10.6.20
+
+---
+
+## About The Project
+
+---
+
+![alt text](Assets/Images/workdayplanner1.png)
+
+![alt text](Assets/Images/workdayplanner2.png)
+
+Implemented the code to workday planner application.
+Once user open the application,the time block's colors will be displayed as below:
+
+-    if current time equals to time block time,then green color will be displayed.
+-    if current time less than to time block time,then gray color will be displayed.
+-    if current time greater than to time block time,then red color will be displayed.
+-    after enter text in textarea and click save button ,the item will be saved in localstorage with corresponding time block.
+-    after page refresh the event will be displayed
+
+## script.js
+
+-    Implemented the following functionalities
+-    setUpTimeBlockColors: display the color of the time block depending on time
+-    initializeToDoList : initialize the ToDoList before saves the events in localstorage
+-    renderSchedule : to display time and corresponding events
+-    saveItem : will be called after user clicked save button to save items in localstorage
+
+## index.html
+
+-    Implemented the code for workday planner
+
+## css
+
+-    added different varieties of styles to html pages using css selectors
+-    used id,class and tag selectors
+
+## Built With
+
+-    javascript
+-    JQuery
+-    html
+-    css
+
+## Getting Started
+
+To get a local copy up and running follow below steps.
+
+## Prerequisites
+
+None
+
+## Installation
+
+Clone the repo
+git clone git@github.com:NirmalaAbothu/work-day-scheduler-10.6.20.git
+
+## Credits
+
+Followed the documentation of https://www.w3schools.com/ for jquery and google documents
+
+## License & copyright
+
+Copyright © 2020 Nirmala Abothu
+
+## Deployed project link
+
+[work-day-scheduler](https://nirmalaabothu.github.io/work-day-scheduler-10.6.20/)
